2b7344284f6dc847cbe7f192a03a0b6155507505
laviniafr <laviniafratila@yahoo.com>
2021-11-22 20:46:16 +0000

    Game Class and DAO updates.

--- src/main/java/com/lf/bullsandcows/dao/GameDao.java
+++ src/main/java/com/lf/bullsandcows/dao/GameDao.java
@@ -10,12 +10,28 @@ import java.util.List;
 public interface GameDao {
 
 	/**
+	 * Method to get all the games played so far.
+	 * @return
+	 */
+	List<Game> getAllGames();
+
+	/**
 	 * Method to get all the rounds of a game ID.
 	 * @return
 	 */
 	List<Round> getRounds(int gameId);
 
-	public Game addGame(Game game);
+	/**
+	 * Method to create a new game.
+	 * @param answer - The generated sequence of numbers.
+	 * @param status - The status of the game.
+	 * @return -The new game object.
+	 */
+	public Game newGame(List<Integer> answer, Status status);
+
+//	public Game addGame(Game game);
+
+	public void completeGame(Game game);
 
 	/**
 	 * Method to get a game's stats by id.
@@ -24,21 +40,15 @@ public interface GameDao {
 	 */
 	Game getGameById(int id);
 
-	/**
-	 * Method to generate a random sequence of 4 distinct digits.
-	 * @return
-	 */
-	List<Integer> generateAnswer();
-
-	/**
-	 * Method to update the status of the game.
-	 */
-	void updateStatus(Status status, int gameId);
-
-	/**
-	 * Method to update the elapsed time of a game.
-	 */
-	void updateElapsedTime(Instant startTime, Instant endTime, int gameId);
+//	/**
+//	 * Method to update the status of the game.
+//	 */
+//	void updateStatus(Status status, int gameId);
+//
+//	/**
+//	 * Method to update the elapsed time of a game.
+//	 */
+//	void updateElapsedTime(Instant startTime, Instant endTime, int gameId);
 
 
 }
